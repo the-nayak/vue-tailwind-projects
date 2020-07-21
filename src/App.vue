da4053029e47b3499c08519518864fc012985f7f
the-nayak <nayak54325@gmail.com>
2020-07-21 23:35:15 +0530

    home page design & code using tailwind css is completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,152 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="bg-gray-100">
+    <section class="overflow-hidden h-screen">
+      <div class="container mx-auto">
+        <div class="relative mt-4 ml-4 sm:ml-24 w-65">
+          <a  href="tel:7470407696">
+            <button class="w-65 h-12 text-2xl absolute z-10 text-blue-900  font-semibold bg-white shadow-lg rounded-xl transition duration-500 ease-in-out appearance-none focus:outline-none focus:bg-yellow-300 hover:bg-gray-200">Contact me</button>
+          </a>
+            <svg xmlns="http://www.w3.org/2000/svg" class="absolute top-0 right-0 mt-8 -mr-24 " width="199.344" height="132.896" viewBox="0 0 199.344 132.896">
+              <g id="button-bg" transform="translate(15.81 -53.604)">
+                <path id="Path_27" data-name="Path 27" d="M66.448,0V66.448A66.448,66.448,0,0,1,0,132.9H0V66.448A66.448,66.448,0,0,1,66.448,0Z" transform="translate(183.534 60.604) rotate(90)" fill="#f0f1f1"/>
+                <path id="Path_28" data-name="Path 28" d="M33.224,0V32.942A33.251,33.251,0,0,1,0,66.219H0V33.277A33.25,33.25,0,0,1,33.224,0Z" transform="translate(116.856 60.604) rotate(90)" fill="#6ed47c"/>
+                <path id="Path_35" data-name="Path 35" d="M132.9,66.448H66.448A66.448,66.448,0,0,1,0,0H66.448A66.448,66.448,0,0,1,132.9,66.448Z" transform="translate(50.638 53.604) rotate(90)" fill="#f0f1f1"/>
+                <path id="Path_36" data-name="Path 36" d="M66.219,33.224H33.277A33.251,33.251,0,0,1,0,0H32.942A33.25,33.25,0,0,1,66.219,33.224Z" transform="translate(50.638 53.604) rotate(90)" fill="#ffc700"/>
+              </g>
+            </svg>
+        </div>
+      </div>
+      <div class="">
+        <div class="flex justify-between align-center">
+          <h1 class="md:text-6xl text-4xl  static font-semibold mt-48 ml-3 md:mt-56 md:ml-56 text-blue-900 leading-tight"> Below are some Projects I've  worked on</h1>
+          <img :src="mobileBg" class="-mt-4 staticfloat-right -mr-4 h-screen sm:hidden" alt="">
+          <img :src="desktopBg" class="-mt-4 hidden sm:block float-right h-screen" alt="">
+        </div>
+      </div>
+      <div class="absolute bottom-0">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-32 w-56 hidden md:block" viewBox="0 0 277.19 138.595">
+          <path id="Path_23" data-name="Path 23" d="M600,0V138.595A138.6,138.6,0,0,0,738.595,277.19h0V138.595A138.6,138.6,0,0,0,600,0Z" transform="translate(0 738.595) rotate(-90)" fill="#ff743c" opacity="0.8"/>
+        </svg>
+      </div>
+    </section> <br><br> <br>
+    <!-- section cards begins here -->
+    <section>
+      <div class="w-3/4 mx-auto flex justify-between flex-wrap">
+        <div class="relative my-32 w-330  bg-gray-100 shadow-lg rounded-lg">
+          <a href="#" class="cursor-pointer">
+          <img src="https://dummyimage.com/330x250/a6a6a6/fff" class="rounded-t-xl z-50 relative object-cover" alt="" style="z-index:50;">
+            <div class="z-20 p-4 bg-white rounded-b-xl relative">
+              <h1 class="text-2xl font-semibold text-blue-900">Project name</h1>
+              <p class="text-base text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum quos magnam id cupiditate dicta? Facilis consequuntur harum inventore animi illo.</p>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" class="absolute top-0 left-0 z-0 -ml-12 -mt-12" width="212.259" height="106.129" viewBox="0 0 212.259 106.129">
+              <path id="yellow-leaf" d="M600,0V106.129A106.13,106.13,0,0,0,706.129,212.259h0V106.129A106.13,106.13,0,0,0,600,0Z" transform="translate(0 706.129) rotate(-90)" fill="#ffc700" opacity="0.7"/>
+            </svg>
+
+            <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-0 bottom-0 z-0 -mr-12 -mb-12" width="212.259" height="106.129" viewBox="0 0 212.259 106.129">
+              <path id="green-leaf" d="M600,0V106.129A106.13,106.13,0,0,0,706.129,212.259h0V106.129A106.13,106.13,0,0,0,600,0Z" transform="translate(0 706.129) rotate(-90)" fill="#6ed47c" opacity="0.7"/>
+            </svg>
+          </a>
+        </div>      
+               
+          <div class="relative my-32 w-330  bg-gray-100 shadow-lg rounded-lg">
+            <img src="https://dummyimage.com/330x250/a6a6a6/fff" class="rounded-t-xl z-50 relative object-cover" alt="" style="z-index:50;">
+              <div class="z-20 p-4 bg-white rounded-b-xl relative">
+                <h1 class="text-2xl font-semibold text-blue-900">Project name</h1>
+                <p class="text-base text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum quos magnam id cupiditate dicta? Facilis consequuntur harum inventore animi illo.</p>
+              </div>
+              <svg xmlns="http://www.w3.org/2000/svg" class="absolute top-0 left-0 z-0 -ml-12 -mt-12" width="212.259" height="106.129" viewBox="0 0 212.259 106.129">
+                <path id="yellow-leaf" d="M600,0V106.129A106.13,106.13,0,0,0,706.129,212.259h0V106.129A106.13,106.13,0,0,0,600,0Z" transform="translate(0 706.129) rotate(-90)" fill="#ffc700" opacity="0.7"/>
+              </svg>
+
+              <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-0 bottom-0 z-0 -mr-12 -mb-12" width="212.259" height="106.129" viewBox="0 0 212.259 106.129">
+                <path id="green-leaf" d="M600,0V106.129A106.13,106.13,0,0,0,706.129,212.259h0V106.129A106.13,106.13,0,0,0,600,0Z" transform="translate(0 706.129) rotate(-90)" fill="#6ed47c" opacity="0.7"/>
+              </svg>
+          </div>      
+               
+          <div class="relative my-32 w-330  bg-gray-100 shadow-lg rounded-lg">
+            <img src="https://dummyimage.com/330x250/a6a6a6/fff" class="rounded-t-xl z-50 relative object-cover" alt="" style="z-index:50;">
+              <div class="z-20 p-4 bg-white rounded-b-xl relative">
+                <h1 class="text-2xl font-semibold text-blue-900">Project name</h1>
+                <p class="text-base text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum quos magnam id cupiditate dicta? Facilis consequuntur harum inventore animi illo.</p>
+              </div>
+              <svg xmlns="http://www.w3.org/2000/svg" class="absolute top-0 left-0 z-0 -ml-12 -mt-12" width="212.259" height="106.129" viewBox="0 0 212.259 106.129">
+                <path id="yellow-leaf" d="M600,0V106.129A106.13,106.13,0,0,0,706.129,212.259h0V106.129A106.13,106.13,0,0,0,600,0Z" transform="translate(0 706.129) rotate(-90)" fill="#ffc700" opacity="0.7"/>
+              </svg>
+
+              <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-0 bottom-0 z-0 -mr-12 -mb-12" width="212.259" height="106.129" viewBox="0 0 212.259 106.129">
+                <path id="green-leaf" d="M600,0V106.129A106.13,106.13,0,0,0,706.129,212.259h0V106.129A106.13,106.13,0,0,0,600,0Z" transform="translate(0 706.129) rotate(-90)" fill="#6ed47c" opacity="0.7"/>
+              </svg>
+          </div>      
+               
+          <div class="relative my-32 w-330  bg-gray-100 shadow-lg rounded-lg">
+            <img src="https://dummyimage.com/330x250/a6a6a6/fff" class="rounded-t-xl z-50 relative object-cover" alt="" style="z-index:50;">
+              <div class="z-20 p-4 bg-white rounded-b-xl relative">
+                <h1 class="text-2xl font-semibold text-blue-900">Project name</h1>
+                <p class="text-base text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum quos magnam id cupiditate dicta? Facilis consequuntur harum inventore animi illo.</p>
+              </div>
+              <svg xmlns="http://www.w3.org/2000/svg" class="absolute top-0 left-0 z-0 -ml-12 -mt-12" width="212.259" height="106.129" viewBox="0 0 212.259 106.129">
+                <path id="yellow-leaf" d="M600,0V106.129A106.13,106.13,0,0,0,706.129,212.259h0V106.129A106.13,106.13,0,0,0,600,0Z" transform="translate(0 706.129) rotate(-90)" fill="#ffc700" opacity="0.7"/>
+              </svg>
+
+              <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-0 bottom-0 z-0 -mr-12 -mb-12" width="212.259" height="106.129" viewBox="0 0 212.259 106.129">
+                <path id="green-leaf" d="M600,0V106.129A106.13,106.13,0,0,0,706.129,212.259h0V106.129A106.13,106.13,0,0,0,600,0Z" transform="translate(0 706.129) rotate(-90)" fill="#6ed47c" opacity="0.7"/>
+              </svg>
+          </div>      
+               
+          <div class="relative my-32 w-330  bg-gray-100 shadow-lg rounded-lg">
+            <img src="https://dummyimage.com/330x250/a6a6a6/fff" class="rounded-t-xl z-50 relative object-cover" alt="" style="z-index:50;">
+              <div class="z-20 p-4 bg-white rounded-b-xl relative">
+                <h1 class="text-2xl font-semibold text-blue-900">Project name</h1>
+                <p class="text-base text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum quos magnam id cupiditate dicta? Facilis consequuntur harum inventore animi illo.</p>
+              </div>
+              <svg xmlns="http://www.w3.org/2000/svg" class="absolute top-0 left-0 z-0 -ml-12 -mt-12" width="212.259" height="106.129" viewBox="0 0 212.259 106.129">
+                <path id="yellow-leaf" d="M600,0V106.129A106.13,106.13,0,0,0,706.129,212.259h0V106.129A106.13,106.13,0,0,0,600,0Z" transform="translate(0 706.129) rotate(-90)" fill="#ffc700" opacity="0.7"/>
+              </svg>
+
+              <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-0 bottom-0 z-0 -mr-12 -mb-12" width="212.259" height="106.129" viewBox="0 0 212.259 106.129">
+                <path id="green-leaf" d="M600,0V106.129A106.13,106.13,0,0,0,706.129,212.259h0V106.129A106.13,106.13,0,0,0,600,0Z" transform="translate(0 706.129) rotate(-90)" fill="#6ed47c" opacity="0.7"/>
+              </svg>
+          </div>      
+          <div class="relative my-32 w-330  bg-gray-100 shadow-lg rounded-lg">
+            <img src="https://dummyimage.com/330x250/a6a6a6/fff" class="rounded-t-xl z-50 relative object-cover" alt="" style="z-index:50;">
+              <div class="z-20 p-4 bg-white rounded-b-xl relative">
+                <h1 class="text-2xl font-semibold text-blue-900">Project name</h1>
+                <p class="text-base text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum quos magnam id cupiditate dicta? Facilis consequuntur harum inventore animi illo.</p>
+              </div>
+              <svg xmlns="http://www.w3.org/2000/svg" class="absolute top-0 left-0 z-0 -ml-12 -mt-12" width="212.259" height="106.129" viewBox="0 0 212.259 106.129">
+                <path id="yellow-leaf" d="M600,0V106.129A106.13,106.13,0,0,0,706.129,212.259h0V106.129A106.13,106.13,0,0,0,600,0Z" transform="translate(0 706.129) rotate(-90)" fill="#ffc700" opacity="0.7"/>
+              </svg>
+
+              <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-0 bottom-0 z-0 -mr-12 -mb-12" width="212.259" height="106.129" viewBox="0 0 212.259 106.129">
+                <path id="green-leaf" d="M600,0V106.129A106.13,106.13,0,0,0,706.129,212.259h0V106.129A106.13,106.13,0,0,0,600,0Z" transform="translate(0 706.129) rotate(-90)" fill="#6ed47c" opacity="0.7"/>
+              </svg>
+          </div>      
+               
+                    
+      </div>
+    </section> <br><br>
+    <!-- section cards ends here -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data(){
+    return{
+      mobileBg: require('./assets/img/mobile-bg.svg'),
+      desktopBg: require('./assets/img/hero.svg'),
+
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
